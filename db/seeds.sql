b3310c7b0953f06116af1660639ec1c248209b3c
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-17 15:38:22 -0700

    templates inserted

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("House Burger", False);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", False);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", False);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", False);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", False);
