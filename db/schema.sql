a8135f876b09311ef9e52c77cce167bcd029598a
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-19 13:59:07 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,7 @@ USE burgers_db;
 
 CREATE TABLE  burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN,
-	PRIMARY KEY (id)
+    burger_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false NOT NULL
 );
