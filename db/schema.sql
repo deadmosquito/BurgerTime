22ceb7114329998f4cf7ce1c45d056a5761c1682
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-27 11:53:17 -0700

    added the models folder with burger file

--- db/schema.sql
+++ db/schema.sql
@@ -7,4 +7,5 @@ CREATE TABLE  burgers
     burger_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false NOT NULL
+    PRIMARY KEY (id)
 );
