bf4b4804e3856abe585b62c47b0550f3a2d7e627
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-27 14:59:47 -0700

    it works!

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,13 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+
+} else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -8,6 +15,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,11 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
+
 CREATE TABLE burgers
 (
-    burger_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR
-    (50) NOT NULL,
-    devoured BOOLEAN DEFAULT false NOT NULL
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
